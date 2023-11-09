b22da181d03fc7e75fed6b50880fe68154fd95be
Adrian Karma <adrian.karma.chan@gmail.com>
2023-11-09 00:55:15 -0300

    Update 02-Ejercicio.js
    agrege un listado, para mejorar la devolucion del array

--- js/02-Ejercicio.js
+++ js/02-Ejercicio.js
@@ -16,5 +16,13 @@ document.write(`<p>El elemento que ocupa la segunda posicion es: ${ciudades[1]}<
 
 ciudades[1] = "Barcelona"; //sobreescribo 
 
-document.write(`<p>El array actualizado es: ${ciudades}</p>`); //quinto
-j
+document.write(`<p>El arreglo actualizado es: ${ciudades}</p>`);
+
+document.write("<h1>Lista completa de ciudades actualizadas:</h1>");
+document.write("<ul>");
+
+for (let i = 0; i < ciudades.length; i++) {
+  document.write(`<li>${ciudades[i]}</li>`);
+}
+
+document.write("</ul>"); //quinto
